0789668c184ebfd6b83a9dd6db3efe7456f2d850
Rattakarn Noppanutchaiyanan <60143418@g.cmru.ac.th>
2020-07-18 16:23:14 +0700

    add route2

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', "HomeController@index");
-Route::get('/', "HomeController@show");
-Route::get('/', "HomeController@create");
-Route::get('/', "HomeController@store");
-Route::get('/', "HomeController@update");
-Route::get('/', "HomeController@delete");
+Route::get('/show', "HomeController@show");
+Route::get('/create', "HomeController@create");
+// Route::get('/', "HomeController@store");
+// Route::get('/', "HomeController@update");
+// Route::get('/', "HomeController@delete");
 
