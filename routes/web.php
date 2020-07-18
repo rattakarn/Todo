470daf432d7f3ea338633c6308330de0af2bf898
Rattakarn Noppanutchaiyanan <60143418@g.cmru.ac.th>
2020-07-18 16:18:49 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', "HomeController@index");
+Route::get('/', "HomeController@show");
+Route::get('/', "HomeController@create");
+Route::get('/', "HomeController@store");
+Route::get('/', "HomeController@update");
+Route::get('/', "HomeController@delete");
+
